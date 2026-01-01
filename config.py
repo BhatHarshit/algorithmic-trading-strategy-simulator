39d210a651ff36e7286bf2cdc604a619fa7e82c5
BhatHarshit <bhattharshit278@gmail.com>
2026-01-01 14:37:59 +0530

    Recreate virtual environment using Python 3.12

---
+++ config.py
@@ -0,0 +1,11 @@
+STOCKS = [
+    "AAPL", "MSFT", "GOOGL", "AMZN", "META", "NVDA", "TSLA",
+    "JPM", "V", "MA", "UNH", "HD", "PG", "XOM", "BAC",
+    "AVGO", "PEP", "COST", "KO", "ADBE",
+    "NFLX", "INTC", "CSCO", "CRM", "QCOM",
+    "ORCL", "TXN", "AMD", "IBM", "GS",
+    "MS", "WMT", "DIS", "NKE", "UPS",
+    "BA", "CAT", "MMM", "GE", "HON",
+    "LIN", "TMO", "ABT", "CVX", "AXP",
+    "BLK", "SPGI"
+]
