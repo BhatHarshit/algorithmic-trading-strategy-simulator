6e568243368d8242ed4447c76045f9c4c9c924a6
BhatHarshit <bhattharshit278@gmail.com>
2026-01-01 14:43:31 +0530

    Download and store 5 years of data for 50 stocks

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,11 +1,26 @@
 import yfinance as yf
 import pandas as pd
+import os
+from config import STOCKS
 
-def download_stock_data(ticker, start="2019-01-01", end="2024-01-01"):
-    data = yf.download(ticker, start=start, end=end)
-    data.reset_index(inplace=True)
-    return data
+DATA_DIR = "data/raw"
+os.makedirs(DATA_DIR, exist_ok=True)
+
+def download_stock(ticker, start="2019-01-01", end="2024-01-01"):
+    df = yf.download(ticker, start=start, end=end)
+    df["Ticker"] = ticker
+    df.reset_index(inplace=True)
+    return df
+
+def download_all_stocks():
+    all_data = []
+    for ticker in STOCKS:
+        print(f"Downloading {ticker}")
+        df = download_stock(ticker)
+        df.to_csv(f"{DATA_DIR}/{ticker}.csv", index=False)
+        all_data.append(df)
+    return pd.concat(all_data)
 
 if __name__ == "__main__":
-    df = download_stock_data("AAPL")
-    print(df.head())
+    data = download_all_stocks()
+    print(data.head())
